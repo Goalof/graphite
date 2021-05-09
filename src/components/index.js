9da15aeacacd17983567e87e22eb87de0ac5654f
Goalof <68434460+Goalof@users.noreply.github.com>
2021-05-10 02:24:43 +0600

    Changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as MobileSide2 } from "./MobileSide2"
 export { default as Masks } from "./Masks"
 export { default as PopUp } from "./PopUp"
+export { default as QuarklycommunityKitAnimation } from "./QuarklycommunityKitAnimation"
